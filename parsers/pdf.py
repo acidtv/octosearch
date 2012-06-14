2f082214a83de982fdc4801ea96f938ff01732b6
AlexBroccoli <alex@qwoot.net>
2012-06-14 23:27:33 +0200

    added pdf parser

---
+++ parsers/pdf.py
@@ -0,0 +1,20 @@
+
+from pyPdf.pdf import PdfFileReader
+
+class ParserPdf:
+	def types(self):
+		return {
+			'mimetypes': ['application/pdf']
+		}
+
+	def parse(self, file_full):
+		pdf = PdfFileReader(file(file_full, 'rb'))
+
+		pages = pdf.getNumPages()
+		text = ''
+
+		for pagenr in range(pages):
+			page = pdf.getPage(pagenr-1)
+			text += ' ' + page.extractText()
+
+		return text
