88a24974356a7259946bea0ce67db0be6adea8a3
Alex <alex@1200wd.com>
2018-03-10 10:27:05 +0100

    Adding html parser class

---
+++ octosearch/parser/html.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+
+
+class ParserHtml:
+
+    _metadata = {}
+
+    def parse(self, file):
+        content = ''
+
+        with file.open_text() as f:
+            soup = BeautifulSoup(f.read(), 'lxml')
+
+        # get_text() sometimes till returns bits of js, make sure those are gone before
+        # calling get_text()
+        for noindex in soup(['script', 'style']):
+            noindex.decompose()
+
+        content = soup.get_text()
+
+        if soup.title:
+            self._metadata['title'] = soup.title.string
+
+        return content
+
+    def extra(self):
+        return self._metadata
