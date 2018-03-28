773c867e8ccffa3273c79b62c8805316ace1f9eb
Alex <alex@1200wd.com>
2018-03-28 22:47:37 +0200

    Fixing hashlib param err in basic auth plugin

--- octosearch/auth/basic.py
+++ octosearch/auth/basic.py
@@ -12,8 +12,9 @@ class BasicAuth(object):
         if username.strip() == '' or password.strip() == '':
             return False
 
-        pwd_hash = hashlib.md5(password).hexdigest()
+        pwd_hash = hashlib.md5(password.encode()).hexdigest()
 
+        print(pwd_hash)
         if username == self._conf['username'] and pwd_hash == self._conf['password_hash']:
             return True
 
