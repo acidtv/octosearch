3451fd7db3c9296beaa14f4a7899ad2fa9704580
Alex <alex@1200wd.com>
2018-05-11 12:26:03 +0200

    Switching es to parallel_bulk indexing with 4 threads

--- octosearch/backends/elasticsearch.py
+++ octosearch/backends/elasticsearch.py
@@ -65,10 +65,11 @@ class BackendElasticSearch:
 
         conn = connections.get_connection()
 
-        for result in elasticsearch.helpers.streaming_bulk(
+        for result in elasticsearch.helpers.parallel_bulk(
             client=conn,
             actions=self._bulk_generator(documents),
-            chunk_size=self._bulk_chunk_size
+            chunk_size=self._bulk_chunk_size,
+            thread_count=4
         ):
             pass
 
